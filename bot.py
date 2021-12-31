4d5251565ea1d771b5b78acd4fd464c6ee384dd3
Swahili2 <53920453+Swahili2@users.noreply.github.com>
2021-12-31 07:21:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,4 @@
-import logging
-import logging.config
 
-# Get logging configurations
-logging.config.fileConfig('logging.conf')
-logging.getLogger().setLevel(logging.ERROR)
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
