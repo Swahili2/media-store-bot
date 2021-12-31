c7666a92c123530de8a8c7e995658fe2ed0c3461
Swahili2 <53920453+Swahili2@users.noreply.github.com>
2021-12-31 08:22:36 +0300

    Update Inlinemode.py

--- plugins/Inlinemode.py
+++ plugins/Inlinemode.py
@@ -67,7 +67,7 @@ async def answer(bot, query):
             else:
                 reply_markup=get_reply_markup(string,file.file_id,nyva)
                 results.append(InlineQueryResultCachedPhoto(
-                        id = uuid4()
+                        id = uuid4(),
                         photo_file_id = file.mime_type,
                         title=title,
                         description= descp,
@@ -86,7 +86,7 @@ async def answer(bot, query):
                            switch_pm_parameter="start",
                            next_offset=str(next_offset))
         except Exception as e:
-            
+            logging.exception(str(e))
             await query.answer(results=[], is_personal=True,
                            cache_time=cache_time,
                            switch_pm_text=str(e)[:63],
@@ -110,3 +110,4 @@ def get_reply_markup(query, file_id, nyva):
             InlineKeyboardButton('📤 Download', url=f"https://telegram.dog/{nyva}?start=subinps_-_-_-_{file_id}")
         ]
         ]
+    return InlineKeyboardMarkup(buttons)
