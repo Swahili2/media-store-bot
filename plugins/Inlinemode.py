bd3b1f6e9cf256bec341d3b7243fa7a743ad870b
Swahili2 <53920453+Swahili2@users.noreply.github.com>
2022-01-01 08:16:54 +0300

    Update Inlinemode.py

--- plugins/Inlinemode.py
+++ plugins/Inlinemode.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 cache_time = 0 if AUTH_USERS or AUTH_CHANNEL else CACHE_TIME
 
 BOT = {}
-@Client.on_inline_query(filters.group)
+@Client.on_inline_query()
 async def answer(bot, query):
     """Show search results for given inline query"""
     nyva=BOT.get("username")
