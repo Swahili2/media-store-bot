62c5e8a61273d7f4cd55565ca14b28b17a830b92
Swahili2 <53920453+Swahili2@users.noreply.github.com>
2022-01-04 19:44:13 +0300

    Update group.py

--- plugins/group.py
+++ plugins/group.py
@@ -5,7 +5,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQ
 from pyrogram import Client, filters
 import re
 from pyrogram.errors import UserNotParticipant
-from utils import get_filter_results, get_file_details, is_subscribed, get_poster
+from utils import get_filter_results, get_file_details, is_subscribed
 BUTTONS = {}
 BOT = {}
 
